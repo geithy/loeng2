2356b13a33decdc63beaeff7804136b00208a3c9
geitplak <geitplak@greeny.cs.tlu.ee>
2016-09-14 08:55:37 +0300

    sõnum muudatustest

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<?php
+
+$firstName = "Geithy";
+$lastName = "Plakk";
+//echo trükib välja muutuja väärtuse greeny.cs.tlu.ee/~geithy lehele - ja näeme seda nime "Geithy" sellel lehel
+//echo $firstName;
+//echo $lastName;
+echo $firstName." ".$lastName;
+//punkt liidab stringe, ja " " teeb tühiku teksti sees.
+
+
+
+?>
